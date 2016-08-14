dded9935d1a4c58616b99924d9e6a6c88494f185
Arvid Norberg <arvid.norberg@gmail.com>
2016-08-14 11:36:21 -0400

    use memory mapped files for the peer buffer (#12)
    use memory mapped files for the peer buffer, to preserve state between restarts

---
+++ mapped_file.hpp
@@ -0,0 +1,147 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Arvid Norberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <fcntl.h> // for open
+#include <sys/mman.h> // for mmap
+#include <errno.h>
+#include <system_error>
+#include <cassert>
+#include <unistd.h> // for open/close
+#include <cstring> // for memcpy
+
+struct file
+{
+	file(char const* name)
+	{
+		m_fd = open(name, O_CREAT | O_RDWR, 0700);
+		if (m_fd < 0) {
+			throw std::system_error(std::error_code(errno, std::generic_category()));
+		}
+	}
+	file(file const&) = delete;
+	file& operator=(file const&) = delete;
+
+	int fd() const { return m_fd; }
+
+	~file() { close(m_fd); }
+
+private:
+	int m_fd;
+};
+
+struct mapped_file
+{
+	mapped_file(char const* file, size_t const size)
+		: m_file(file)
+		, m_buf(nullptr)
+		, m_size(size)
+	{
+		if (ftruncate(m_file.fd(), m_size) < 0) {
+			throw std::system_error(std::error_code(errno, std::generic_category()));
+		}
+		m_buf = mmap(nullptr, m_size
+			, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FILE, m_file.fd(), 0);
+		if (m_buf == MAP_FAILED) {
+			throw std::system_error(std::error_code(errno, std::generic_category()));
+		}
+	}
+	mapped_file(mapped_file const&) = delete;
+	mapped_file& operator=(mapped_file const&) = delete;
+
+	void* data() { return m_buf; }
+	void const* data() const { return m_buf; }
+	size_t size() const { return m_size; }
+
+	~mapped_file() { munmap(m_buf, m_size); }
+
+private:
+
+	file m_file;
+	void* m_buf;
+	size_t const m_size;
+};
+
+template <typename T>
+struct mapped_vector
+{
+	// the header must be large enough to make the first element still be
+	// correctly aligned
+	static constexpr size_t header_size = (16 > alignof(T)) ? alignof(T) : 16;
+
+	mapped_vector(char const* file, size_t const size)
+		: m_map(file, header_size + size * sizeof(T))
+		, m_size(*static_cast<size_t*>(m_map.data()))
+	{
+		// the actual array starts immediately after the header
+		m_array = reinterpret_cast<T*>(static_cast<char*>(m_map.data()) + header_size);
+
+		// Make sure to cap the size of the vector within its capacity
+		if (m_size > capacity()) m_size = capacity();
+	}
+
+	void resize(size_t const s)
+	{
+		if (s == m_size) return;
+		if (s < m_size) m_size = s;
+		assert(false && "support default constructing new items");
+	}
+	bool empty() const { return m_size == 0; }
+
+	T* data() { return m_array; }
+	T const* data() const { return m_array; }
+	size_t size() const { return m_size; }
+	size_t capacity() const { return (m_map.size() - header_size) / sizeof(T); }
+
+	template<typename... Args>
+	void emplace_back(Args... args)
+	{
+		if (m_size >= capacity()) throw std::range_error("mapped_vector::emplace_back() full");
+
+		new (&m_array[m_size]) T(std::forward<Args>(args)...);
+		++m_size;
+	}
+
+	T& operator[](int const idx)
+	{
+		assert(idx < m_size);
+		assert(idx >= 0);
+		return m_array[idx];
+	}
+
+	T const& operator[](int const idx) const
+	{
+		assert(idx < m_size);
+		assert(idx >= 0);
+		return m_array[idx];
+	}
+
+private:
+	mapped_file m_map;
+	T* m_array;
+	// number of items in use, not capacity. This points into the header of the
+	// mapped file
+	size_t& m_size;
+};
+
---
+++ tests/test_mapped_vector.cpp
@@ -0,0 +1,64 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Arvid Norberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "mapped_file.hpp"
+#include <sys/stat.h>
+#include <unistd.h> // for unlink
+
+int main()
+{
+	unlink("test_file2");
+	{
+		mapped_vector<int> mv("test_file2", 30);
+		assert(mv.size() == 0);
+		assert(mv.capacity() == 30);
+
+		for (int i = 0; i < 10; ++i) {
+			mv.emplace_back(i);
+		}
+
+		assert(mv.size() == 10);
+	}
+
+	{
+		// make sure the size of the vector (not the capacity) is correctly
+		// restored when loading it back up again
+		mapped_vector<int> mv("test_file2", 30);
+		assert(mv.size() == 10);
+		assert(mv.capacity() == 30);
+		for (int i = 0; i < 10; ++i) {
+			assert(mv[i] == i);
+		}
+	}
+
+	// make sure the size is capped at the capacity, if the capacity is shrunk
+	{
+		mapped_vector<int> mv("test_file2", 5);
+		assert(mv.size() == 5);
+		assert(mv.capacity() == 5);
+	}
+
+	unlink("test_file2");
+}
+
+
